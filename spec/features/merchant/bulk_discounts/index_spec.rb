5567e9a437e8458545706894c31a4a185519e560
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-23 17:23:49 -0600

    Create Merchant Bulk Discount Delete specs

--- spec/features/merchant/bulk_discounts/index_spec.rb
+++ spec/features/merchant/bulk_discounts/index_spec.rb
@@ -19,5 +19,53 @@ RSpec.describe 'Merchant Bulk Discounts Index', type: :feature do
         end
       end
     end
+
+    describe 'Merchant Bulk Discount Delete' do
+      before :each do
+        @merchant1 = Merchant.create!(id: 45, name:"Bob's Baskets")
+
+        @discount1 = BulkDiscount.create!(merchant_id: 45, discount: 20, threshold: 5)
+        @discount2 = BulkDiscount.create!(merchant_id: 45, discount: 30, threshold: 10)
+        @discount3 = BulkDiscount.create!(merchant_id: 45, discount: 50, threshold: 20)
+      end
+      it 'Next to each bulk discount I see a link to delete it' do
+        visit merchant_bulk_discounts_path(@merchant1)
+
+        within "#discount_#{@discount1.id}" do
+          expect(page).to have_link("Delete")
+        end
+
+        within "#discount_#{@discount2.id}" do
+          expect(page).to have_link("Delete")
+        end
+
+        within "#discount_#{@discount3.id}" do
+          expect(page).to have_link("Delete")
+        end
+      end
+
+      describe 'When I click this link' do
+        it 'I am redirected back to the bulk discounts index page and I no longer see that discount listed' do
+          visit merchant_bulk_discounts_path(@merchant1)
+
+          within "#discount_#{@discount1.id}" do
+            click_link "Delete"
+          end
+
+          expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+
+          expect(page).to_not have_content("Discount ##{@discount1.id}")
+          expect(page).to_not have_content("20% off 5 items or more")
+
+          within "#discount_#{@discount2.id}" do
+            expect(page).to have_content("30% off 10 items or more")
+          end
+
+          within "#discount_#{@discount3.id}" do
+            expect(page).to have_content("50% off 20 items or more")
+          end
+        end
+      end
+    end
   end
 end
