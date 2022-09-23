019a34deed8c979d6e410fef90354c46b1529812
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-23 14:45:07 -0600

    Create functionality for creating new bulk discount

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -5,4 +5,23 @@ class Merchant::BulkDiscountsController < ApplicationController
 
   def show
   end
+
+  def new
+    @bulk_discount = BulkDiscount.new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new(bulk_discount_params)
+    if @bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant), notice: "Discount created successfully!"
+    else
+      redirect_to new_merchant_bulk_discount_path(@merchant), notice: "Discount not created: Missing required information."
+    end
+  end
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:discount, :threshold, :merchant_id)
+  end
 end
