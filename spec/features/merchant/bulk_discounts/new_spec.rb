da563ebdb33bf479e19abbdbcb07ae946bc3f43a
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-23 16:17:12 -0600

    Add sad path testing

--- spec/features/merchant/bulk_discounts/new_spec.rb
+++ spec/features/merchant/bulk_discounts/new_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Merchant Bulk Discount Create', type: :feature do
       expect(current_path).to eq(merchant_bulk_discounts_path(Merchant.first))
     end
 
-    it 'I see my new bulk discount listed' do
+    it 'I see my new bulk discount listed and I see a message that says "Discount created successfully!"' do
       visit new_merchant_bulk_discount_path(Merchant.first)
 
       fill_in('Percent Discount', with: '45')
@@ -20,6 +20,20 @@ RSpec.describe 'Merchant Bulk Discount Create', type: :feature do
       click_button 'Create'
 
       expect(page).to have_content("45% off 10 items or more")
+      expect(page).to have_content("Discount created successfully!")
+    end
+  end
+
+  describe 'When I do not fill in all of the forms with valid data' do
+    it 'I am redirected to the same page and I see a message that says "Discount not created: Missing required information."' do
+      visit new_merchant_bulk_discount_path(Merchant.first)
+
+      fill_in('Percent Discount', with: '45')
+      #'Item Threshold' is not filled in
+      click_button 'Create'
+
+      expect(current_path).to eq(new_merchant_bulk_discount_path(Merchant.first))
+      expect(page).to have_content("Discount not created: Missing required information.")
     end
   end
 end
