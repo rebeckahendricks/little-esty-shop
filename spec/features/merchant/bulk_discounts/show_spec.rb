73721bb88240793fe90c55f5ca406739df512407
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-23 17:56:03 -0600

    Create bulk discount show page spec

---
+++ spec/features/merchant/bulk_discounts/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Buld Discount Show', type: :feature do
+  before :each do
+    @merchant1 = Merchant.create!(id: 45, name:"Bob's Baskets")
+
+    @discount1 = BulkDiscount.create!(merchant_id: 45, discount: 20, threshold: 5)
+    @discount2 = BulkDiscount.create!(merchant_id: 45, discount: 30, threshold: 10)
+  end
+
+  describe 'As a merchant' do
+    describe 'When I visit my bulk discount show page' do
+      it 'I see the bulk discounts quantity threshold and percentage discount' do
+        visit merchant_bulk_discount_path(@merchant1, @discount1)
+
+        expect(page).to have_content("Discount ##{@discount1.id}")
+        expect(page).to have_content("20% off 5 items or more")
+
+        expect(page).to_not have_content("Discount ##{@discount2.id}")
+        expect(page).to_not have_content("30% off 10 items or more")
+
+      end
+    end
+  end
+end
