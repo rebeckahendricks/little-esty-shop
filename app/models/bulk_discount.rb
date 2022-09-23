d2bcca00053df03f8771ba33ded667111c6c3d9f
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-23 12:43:22 -0600

    Add merchant relationship in spec, add validations and relationships to BulkDiscount model

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,2 +1,6 @@
 class BulkDiscount < ApplicationRecord
+  belongs_to :merchant
+
+  validates :discount, presence: true, numericality: true
+  validates :threshold, presence: true, numericality: true
 end
