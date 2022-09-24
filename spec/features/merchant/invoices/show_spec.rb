ae27c9d1742b96478eac1810247f111429086565
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-24 08:53:07 -0600

    Fix: Within blocks in merchant invoices show spec

--- spec/features/merchant/invoices/show_spec.rb
+++ spec/features/merchant/invoices/show_spec.rb
@@ -23,36 +23,39 @@ RSpec.describe 'Merchant Invoices Show Page' do
 
         visit merchant_invoice_path(@merchant, invoice_1)
 
-        within "div#1" do
-          expect(page).to have_content("Item Name: Item Qui Esse")
-          expect(page).to have_content("Item Quantity: 5")
-          expect(page).to have_content("Item Unit Price: $136.35")
-
-          expect(page).to_not have_content("Item Name: Item Expedita Aliquam")
-          expect(page).to_not have_content("Item Name: Provident At")
+        within "#name_#{invoice_1.invoice_items.first.id}" do
+          expect(page).to have_content("Item Qui Esse")
         end
+
+        within "#quantity_#{invoice_1.invoice_items.first.id}" do
+          expect(page).to have_content("5")
+        end
+
+        within "#price_#{invoice_1.invoice_items.first.id}" do
+          expect(page).to have_content("$136.35")
+        end
+
+        expect(page).to_not have_content("Item Name: Item Expedita Aliquam")
+        expect(page).to_not have_content("Item Name: Provident At")
       end
 
       it 'I see the total revenue that will be generated from all of my items on the invoice' do
         @merchant = Merchant.first
         invoice_1 = @merchant.invoices.first
-        invoice_2 = @merchant.invoices.last
 
         visit merchant_invoice_path(@merchant, invoice_1)
-          expect(page).to have_content("Total Revenue: $21067.77")
+
+        expect(page).to have_content("Total Revenue: $21067.77")
       end
 
       it 'I see that each invoice item status is a select field and I see that the invoice items current status is selected' do
         @merchant = Merchant.first
         invoice_1 = @merchant.invoices.first
-        invoice_2 = @merchant.invoices.last
 
         visit merchant_invoice_path(@merchant, invoice_1)
 
-        within "div#1" do
-          within "#status_#{invoice_1.invoice_items.first.id}" do
-            expect(page).to have_select('status'), 'packaged'
-          end
+        within "#status_#{invoice_1.invoice_items.first.id}" do
+          expect(page).to have_select('status'), 'packaged'
         end
       end
 
@@ -60,30 +63,24 @@ RSpec.describe 'Merchant Invoices Show Page' do
         it 'I can select a new status for the Item' do
           @merchant = Merchant.first
           invoice_1 = @merchant.invoices.first
-          invoice_2 = @merchant.invoices.last
 
           visit merchant_invoice_path(@merchant, invoice_1)
 
-          within "div#1" do
-            within "#status_#{invoice_1.invoice_items.first.id}" do
-              select :shipped, from: 'status'
+          within "#status_#{invoice_1.invoice_items.first.id}" do
+            select :shipped, from: 'status'
 
-              expect(page).to have_select('status'), 'shipped'
-            end
+            expect(page).to have_select('status'), 'shipped'
           end
         end
 
         it 'next to the select field I see a button to "Update Item Status"' do
           @merchant = Merchant.first
           invoice_1 = @merchant.invoices.first
-          invoice_2 = @merchant.invoices.last
 
           visit merchant_invoice_path(@merchant, invoice_1)
 
-          within "div#1" do
-            within "#status_#{invoice_1.invoice_items.first.id}" do
-              expect(page).to have_button('Update Item Status')
-            end
+          within "#status_#{invoice_1.invoice_items.first.id}" do
+            expect(page).to have_button('Update Item Status')
           end
         end
 
@@ -91,32 +88,26 @@ RSpec.describe 'Merchant Invoices Show Page' do
           it 'I am taken back to the merchant invoice show page' do
             @merchant = Merchant.first
             invoice_1 = @merchant.invoices.first
-            invoice_2 = @merchant.invoices.last
 
             visit merchant_invoice_path(@merchant, invoice_1)
 
-            within "div#1" do
-              within "#status_#{invoice_1.invoice_items.first.id}" do
-                select :shipped, from: 'status'
-                click_button 'Update Item Status'
-                expect(current_path).to eq(merchant_invoice_path(@merchant, invoice_1))
-              end
+            within "#status_#{invoice_1.invoice_items.first.id}" do
+              select :shipped, from: 'status'
+              click_button 'Update Item Status'
+              expect(current_path).to eq(merchant_invoice_path(@merchant, invoice_1))
             end
           end
 
           it 'I see that my Items status has now been updated' do
             @merchant = Merchant.first
             invoice_1 = @merchant.invoices.first
-            invoice_2 = @merchant.invoices.last
 
             visit merchant_invoice_path(@merchant, invoice_1)
 
-            within "div#1" do
-              within "#status_#{invoice_1.invoice_items.first.id}" do
-                select :shipped, from: 'status'
-                click_button 'Update Item Status'
-                expect(page).to have_select('status'), 'shipped'
-              end
+            within "#status_#{invoice_1.invoice_items.first.id}" do
+              select :shipped, from: 'status'
+              click_button 'Update Item Status'
+              expect(page).to have_select('status'), 'shipped'
             end
           end
         end
