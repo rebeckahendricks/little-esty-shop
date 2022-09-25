e39b6ccd89f0a8bd84532d33907c70bb7241349f
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-25 10:36:31 -0600

    Create relationships between invoice_items and bulk_discounts

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,6 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  has_many :invoice_items
 
   validates :discount, presence: true, numericality: true
   validates :threshold, presence: true, numericality: true
