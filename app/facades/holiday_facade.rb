46af01a9f5b63803db23e6ab5098cc93f7bd89aa
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-26 18:01:36 -0600

    Delete unused code

--- app/facades/holiday_facade.rb
+++ app/facades/holiday_facade.rb
@@ -7,14 +7,4 @@ class HolidayFacade
       Holiday.new(holiday_data)
     end
   end
-
-  # def self.next_three_holidays
-  #   data = HolidayService.get_holidays
-  #   holidays = []
-  #   until holidays.count == 3
-  #     data.each do |holiday_data|
-  #       holidays << Holiday.new(holiday_data)
-  #     end
-  #   end
-  # end
 end
